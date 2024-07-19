87bd6b02b709ce5dd719eb0f3771777fb0585f6c
mandla <aldnammandla@live.com>
2024-07-19 07:44:28 +0200

    add theme  and update index.tsx

--- stroller-frontend/src/index.tsx
+++ stroller-frontend/src/index.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import AppRoutes from "./routes";
+import { ThemeProvider } from "@mui/styles";
+import theme from "./theme";
 ReactDOM.render(
   <React.StrictMode>
-    <AppRoutes />
+    <ThemeProvider theme={theme}>
+      <AppRoutes />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ stroller-frontend/src/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material/styles";
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1976D2",
+    },
+    secondary: {
+      main: "#DC004E",
+    },
+  },
+});
+export default theme;
